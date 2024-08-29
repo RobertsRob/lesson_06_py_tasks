8342fb9d540a53788b8e31552e698180e6243dde
RobertsRob <83211059+RobertsRob@users.noreply.github.com>
2024-08-29 20:33:33 +0300

    Update tasks_2.py

--- tasks_2.py
+++ tasks_2.py
@@ -1,8 +1,21 @@
 # 1.task
+# Создайте список из 6 чисел и отсортируйте его в порядке убывания с использованием метода sort.
+numbers = [5, 2, 9, 1, 7, 6]
+numbers.sort(reverse=True)
+print(numbers)
 
 # 2.task
+# Создайте список из 5 чисел и найдите индекс максимального элемента, используя функции max и index.
+numbers = [5, 8, 2, 10, 3]
+max_value = max(numbers)
+index = numbers.index(max_value)
+print(index)
 
 # 3.task
+# Напишите программу, которая создает список из 5 чисел и удаляет последний элемент, используя оператор del.
+numbers = [12, 7, 9, 3, 15]
+del numbers[-1]
+print(numbers)
 
 # 4.task
 
